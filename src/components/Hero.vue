64a92f3cd2a96e40f1be39a70dddc49db89f18f5
Paul Doyle <71635533+N00193346@users.noreply.github.com>
2021-12-06 14:53:08 +0000

    Styled all courses page, added unsplash api to retrieve a hero image for the page

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+   <v-container fluid>
+    <v-row justify="space-around">
+      <v-col cols="5">
+        <v-img
+          :src="heroImage.name"
+          aspect-ratio="1.7"
+        ></v-img>
+      </v-col>
+    </v-row>
+   </v-container>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'Hero',
+        props: {
+           heroImage: Object
+        }
+    }
+</script>
+<style scoped>
+
+</style>
--- src/pages/courses/Index.vue
+++ src/pages/courses/Index.vue
@@ -1,33 +1,41 @@
 <template>
 <div>
-  <h1 class="subheading">Courses </h1>
+
   <v-container class="my-5">
-  <!-- <b-col>
-    <h2>Welcome to the Course Index page</h2>
+  <h1 class="subheading">Courses </h1>
 
-    <p
-     v-for="course in courses"
-     :key="course._id"
-    >
-  
-
-
-    <router-link :to="{name: 'courses_show', params: {id: course.id}}">{{course.title}}</router-link>
-    </p>
-  </b-col> -->
+<v-layout row >
+    <v-img
+      :src="heroImage"
+      height="350"
+      class=" ma-3"
+    />
+</v-layout>
 
 <v-layout row wrap>
-  <v-flex sm6 lg4 v-for="course in courses" :key="course._id">
+  <v-flex sm6 lg6 v-for="course in courses" :key="course._id">
     <router-link :to="{name: 'courses_show', params: {id: course.id}}">
     <v-card   
     elevation="2"
     outlined  
-    class="text-center ma-3">
-
-      <v-card-text>
-        <div class="heading">{{course.title}}</div>
-   
-      </v-card-text>
+    class=" ma-3">
+      <v-card-title>
+        <div class="textStyle">{{course.title}}</div>
+      </v-card-title>
+      <div class ="d-flex  justify-space-between">
+            <v-card-text  >
+            <div class="textStyle">Code:</div>
+            {{course.code}}
+            </v-card-text>
+             <v-card-text >
+            <div class="textStyle">Points:</div>
+            {{course.points}}
+            </v-card-text>
+             <v-card-text  >
+            <div class="textStyle">Level:</div>
+            {{course.level}}
+            </v-card-text>
+      </div>
     </v-card>
     </router-link>
   </v-flex>
@@ -38,17 +46,26 @@
 
 <script>
 import axios from 'axios'
+// import Hero from "@/components/Hero";
+const UNSPLASH_URL = "https://api.unsplash.com/search/photos/?client_id=XhqXA2Jig1drfBj96ploqpKdat9N94vn0GPzbrYjwK8&query=course";
+
 
 export default {
   name: "CoursesIndex",
-  components: {},
+  components: {
+    // Hero,
+  },
    data() {
        return {
             courses: [],
+            heroImage: {},
+
        }
    },
    mounted() {
        this.getData()
+       this.getHeroImage()
+
    },
    methods: {
        getData() {
@@ -63,8 +80,24 @@ export default {
                this.courses = response.data.data
            })
            .catch(error => console.log(error))
-       }
+       },
+        getHeroImage() {
+      axios
+      .get(`${UNSPLASH_URL}`)
+      .then((response) => {
+          console.log(response);
+          this.heroImage = response.data.results[0].urls.regular;
+          console.log("Image Url is:" + this.heroImage);
+        })
+
+        .catch((error) => console.log(error));
+    },
    },
 
   };
 </script>
+<style scoped>
+.textStyle{
+  font-weight: bold;
+}
+</style>
