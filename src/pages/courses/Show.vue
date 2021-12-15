057bdf87945e83aa0dae08e322f179a5f4adcaa6
Paul Doyle <71635533+N00193346@users.noreply.github.com>
2021-12-15 21:26:38 +0000

    fixed wrong size buttons on show pages

--- src/pages/courses/Show.vue
+++ src/pages/courses/Show.vue
@@ -1,47 +1,41 @@
 <template>
   <div>
-  <v-container class="my-5">
+    <v-container class="my-5">
+      <v-card class="ma-3" elevation="5" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="text-h3 mb-1">
+              {{ course.title }}
+            </v-list-item-title>
+            <v-list-item-subtitle class="fontSize">
+              <div class="subheading">Id:</div>
+              {{ course.id }}
+              <div class="subheading">Code:</div>
+              {{ course.code }}
+              <div class="subheading">Points:</div>
+              {{ course.points }}
+              <div class="subheading">Level:</div>
+              {{ course.level }}
+              <div class="subheading">Description:</div>
+              {{ course.description }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-    <v-card
-    class="ma-3"
-     elevation="5"
-    outlined
+        <v-card-actions>
+          <router-link :to="{ name: 'courses_edit' }">
+            <v-btn
+              outlined
+              rounded
+              text
+              color="white"
+              class="buttonEdit button pa-5"
+            >
+              Edit
+            </v-btn>
+          </router-link>
 
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title class="text-h3 mb-1">
-         {{ course.title}}
-        </v-list-item-title>
-        <v-list-item-subtitle class="fontSize">
-          <div class="subheading">Id:</div>
-            {{ course.id}}
-          <div class="subheading">Code:</div>
-            {{ course.code}}
-          <div class="subheading">Points:</div>
-            {{ course.points}}
-          <div class="subheading">Level:</div>
-            {{ course.level}}
-          <div class="subheading">Description:</div>
-            {{ course.description}}
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-card-actions>
-      <router-link :to="{name: 'courses_edit'}">
-      <v-btn
-        outlined
-        rounded
-        text
-        color="white"
-        class="buttonEdit button pa-5"
-      >
-       Edit
-      </v-btn>
-      </router-link>
-
-        <!-- <v-btn
+          <!-- <v-btn
         @click="deleteCourse()"
         outlined
         rounded
@@ -51,185 +45,181 @@
       >
        Delete
       </v-btn> -->
-      <DeletePopUp :button="this.button" :enrolments="this.enrolments" :type="this.type"/>
-       <div class="errorText" > {{ errors}}</div>
-    </v-card-actions>
-  </v-card>
+          <DeletePopUp
+            :button="this.button"
+            :enrolments="this.enrolments"
+            :type="this.type"
+          />
+          <div class="errorText">{{ errors }}</div>
+        </v-card-actions>
+      </v-card>
 
-  <!-- Enolments -->
-   <v-card
-    class="ma-3"
-     elevation="5"
-    outlined
-  >
-    <v-list-item three-line v-if="this.enrolments.length > 0">
-      <v-list-item-content>
-        <v-list-item-title class="text-h3 mb-5">
-         Course Enrolments
-        </v-list-item-title>
+      <!-- Enolments -->
+      <v-card class="ma-3" elevation="5" outlined>
+        <v-list-item three-line v-if="this.enrolments.length > 0">
+          <v-list-item-content>
+            <v-list-item-title class="text-h3 mb-5">
+              Course Enrolments
+            </v-list-item-title>
 
-  <v-layout row wrap >
-  <v-flex sm6 lg6  v-for="enrolment in enrolments" :key="enrolment._id">
-  
-  <v-card 
-      elevation="2"
-      outlined  
-      class=" ma-5"
-      max-width="500"
-    
-  >
-     <router-link style="text-decoration: none; color: inherit;"
-     :to="{name: 'enrolments_show', params: {id: enrolment.id}}">
-    <v-card-title  class ="d-flex  justify-space-between">
-      {{enrolment.lecturer.name}}
-      <v-chip small :class="`${enrolment.status}`">{{enrolment.status}}</v-chip>
-    </v-card-title>
+            <v-layout row wrap>
+              <v-flex
+                sm6
+                lg6
+                v-for="enrolment in enrolments"
+                :key="enrolment._id"
+              >
+                <v-card elevation="2" outlined class="ma-5" max-width="500">
+                  <router-link
+                    style="text-decoration: none; color: inherit"
+                    :to="{
+                      name: 'enrolments_show',
+                      params: { id: enrolment.id },
+                    }"
+                  >
+                    <v-card-title class="d-flex justify-space-between">
+                      {{ enrolment.lecturer.name }}
+                      <v-chip small :class="`${enrolment.status}`">{{
+                        enrolment.status
+                      }}</v-chip>
+                    </v-card-title>
 
-     <div class ="d-flex  justify-start">
-            <v-card-text  >
-            <div class="textStyle">Enrolment ID:</div>
-            {{enrolment.id}}
-            </v-card-text>
-            <v-card-text  >
-            <div class="textStyle">Course ID:</div>
-            {{enrolment.course_id}}
-            </v-card-text>
-             <v-card-text >
-            <div class="textStyle">Lecturer Id</div>
-             {{enrolment.lecturer.id}}
-            </v-card-text>
-     </div>
-      </router-link>
-
- 
-  </v-card>
-
-  </v-flex>
-</v-layout>
-
-
-      </v-list-item-content>
-    </v-list-item>
-
- 
-  </v-card>
-
-</v-container>
-</div>
+                    <div class="d-flex justify-start">
+                      <v-card-text>
+                        <div class="textStyle">Enrolment ID:</div>
+                        {{ enrolment.id }}
+                      </v-card-text>
+                      <v-card-text>
+                        <div class="textStyle">Course ID:</div>
+                        {{ enrolment.course_id }}
+                      </v-card-text>
+                      <v-card-text>
+                        <div class="textStyle">Lecturer Id</div>
+                        {{ enrolment.lecturer.id }}
+                      </v-card-text>
+                    </div>
+                  </router-link>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import DeletePopUp from "@/components/DeletePopUp"
+import axios from "axios";
+import DeletePopUp from "@/components/DeletePopUp";
 
 export default {
   name: "CoursesShow",
   components: {
-    DeletePopUp
+    DeletePopUp,
   },
-   data() {
-       return {
-            course: {},
-            enrolments: [],
-            type: "course",
-            errors: "",
-            button: {
-              id: this.$route.params.id,
-              class: "button pa-5",
-              message: "Are you sure you want to delete this course?",
-            }
-       }
-   },
-   mounted() {
-       this.getCourse()
-       this.getEnrolments()
-   },
-   methods: {
-       getCourse() {
-
-         let token = localStorage.getItem('token')
-           axios
-           .get(`https://college-api-mo.herokuapp.com/api/courses/${this.$route.params.id}`,
-           {
-               headers: {"Authorization" : `Bearer ${token}`}
-           })
-           .then(response => {
-               console.log(response)
-               this.course = response.data.data
-           })
-           .catch(error => {
-             console.log(error)
-             localStorage.removeItem('token')
-            //  this.$emit('invalid-token')
-             })
-       },
-        getEnrolments() {
-         let token = localStorage.getItem('token')
-           axios
-           .get(`https://college-api-mo.herokuapp.com/api/courses/${this.$route.params.id}`,
-           {
-               headers: {"Authorization" : `Bearer ${token}`}
-           })
-           .then(response => {
-               console.log(response)
-               this.enrolments = response.data.data.enrolments
-               if (this.enrolments.length > 0) {
-                 this.button.message = "Deleting this course will delete all enrolments related to the course, are you sure you want to delete it?"
-               }
-        
-             
-           })
-           .catch(error => {
-             console.log(error)
-  
-             })
-       },
-   },
-
-  };
+  data() {
+    return {
+      course: {},
+      enrolments: [],
+      type: "course",
+      errors: "",
+      button: {
+        id: this.$route.params.id,
+        class: "button pa-5",
+        message: "Are you sure you want to delete this course?",
+      },
+    };
+  },
+  mounted() {
+    this.getCourse();
+    this.getEnrolments();
+  },
+  methods: {
+    getCourse() {
+      let token = localStorage.getItem("token");
+      axios
+        .get(
+          `https://college-api-mo.herokuapp.com/api/courses/${this.$route.params.id}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.course = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+          localStorage.removeItem("token");
+          //  this.$emit('invalid-token')
+        });
+    },
+    getEnrolments() {
+      let token = localStorage.getItem("token");
+      axios
+        .get(
+          `https://college-api-mo.herokuapp.com/api/courses/${this.$route.params.id}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.enrolments = response.data.data.enrolments;
+          if (this.enrolments.length > 0) {
+            this.button.message =
+              "Deleting this course will delete all enrolments related to the course, are you sure you want to delete it?";
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 <style scoped>
-.button{
-   margin-left: 20px;
-   margin-bottom: 10px;
+.button {
+  margin-left: 20px;
+  margin-bottom: 10px;
 }
 
-.buttonEdit{
-   background: #f9aa33 !important;
+.buttonEdit {
+  background: #f9aa33 !important;
 }
 
-.buttonDelete{
-
-   background: red !important;
+.buttonDelete {
+  background: red !important;
 }
 
-.subheading{
+.subheading {
   font-weight: bold;
-  margin-top:10px;
-  margin-bottom:5px;
+  margin-top: 10px;
+  margin-bottom: 5px;
   color: black;
 }
-.fontSize{
+.fontSize {
   font-size: 20px;
   margin: 10px;
 }
 
-.errorText{
+.errorText {
   color: red;
 }
-.v-chip.interested{
+.v-chip.interested {
   background: #f9aa33 !important;
 }
 
-.v-chip.assigned{
-  background: #82b1ff!important;
+.v-chip.assigned {
+  background: #82b1ff !important;
 }
 
-.v-chip.career_break{
+.v-chip.career_break {
   background: red !important;
 }
 
-.v-chip.associate{
+.v-chip.associate {
   background: #4caf50 !important;
 }
 </style>
--- src/pages/lecturers/Show.vue
+++ src/pages/lecturers/Show.vue
@@ -1,44 +1,39 @@
 <template>
-<div>
-  <v-container class="my-5">
-   <v-card
-    class="ma-3"
-     elevation="5"
-    outlined
+  <div>
+    <v-container class="my-5">
+      <v-card class="ma-3" elevation="5" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="text-h3 mb-1">
+              {{ lecturer.name }}
+            </v-list-item-title>
+            <v-list-item-subtitle class="fontSize">
+              <div class="subheading">Id:</div>
+              {{ lecturer.id }}
+              <div class="subheading">Address:</div>
+              {{ lecturer.address }}
+              <div class="subheading">Rmail:</div>
+              {{ lecturer.email }}
+              <div class="subheading">Phone:</div>
+              {{ lecturer.phone }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
 
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title class="text-h3 mb-1">
-         {{ lecturer.name}}
-        </v-list-item-title>
-        <v-list-item-subtitle class="fontSize">
-          <div class="subheading">Id:</div>
-            {{ lecturer.id}}
-          <div class="subheading">Address:</div>
-            {{ lecturer.address}}
-          <div class="subheading">Rmail:</div>
-            {{ lecturer.email}}
-          <div class="subheading">Phone:</div>
-            {{ lecturer.phone}}
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
+        <v-card-actions>
+          <router-link :to="{ name: 'lecturers_edit' }">
+            <v-btn
+              outlined
+              rounded
+              text
+              color="white"
+              class="buttonEdit button pa-5"
+            >
+              Edit
+            </v-btn>
+          </router-link>
 
-    <v-card-actions>
-      <router-link :to="{name: 'lecturers_edit'}">
-      <v-btn
-        outlined
-        rounded
-        text
-        color="white"
-        class="buttonEdit button"
-      >
-       Edit
-      </v-btn>
-      </router-link>
-
-        <!-- <v-btn        
+          <!-- <v-btn        
         outlined
         rounded
         text
@@ -48,186 +43,181 @@
        Delete
       </v-btn> -->
 
-      <DeletePopUp :button="this.button" :enrolments="this.enrolments" :type="this.type"/>
-      <div class="errorText" > {{ errors}}</div>
+          <DeletePopUp
+            :button="this.button"
+            :enrolments="this.enrolments"
+            :type="this.type"
+          />
+          <div class="errorText">{{ errors }}</div>
+        </v-card-actions>
+      </v-card>
 
+      <v-card class="ma-3" elevation="5" outlined>
+        <v-list-item three-line v-if="this.enrolments.length > 0">
+          <v-list-item-content>
+            <v-list-item-title class="text-h3 mb-5">
+              {{ lecturer.name }}'s Enrolments
+            </v-list-item-title>
 
-    </v-card-actions>
-  </v-card>
-  
-     <v-card
-    class="ma-3"
-     elevation="5"
-    outlined
-  >
-    <v-list-item three-line v-if="this.enrolments.length > 0">
-      <v-list-item-content>
-        <v-list-item-title class="text-h3 mb-5">
-         {{ lecturer.name}}'s Enrolments
-        </v-list-item-title>
+            <v-layout row wrap>
+              <v-flex
+                sm6
+                lg6
+                v-for="enrolment in enrolments"
+                :key="enrolment._id"
+              >
+                <v-card elevation="2" outlined class="ma-5" max-width="500">
+                  <router-link
+                    style="text-decoration: none; color: inherit"
+                    :to="{
+                      name: 'enrolments_show',
+                      params: { id: enrolment.id },
+                    }"
+                  >
+                    <v-card-title class="d-flex justify-space-between">
+                      {{ enrolment.course.title }}
+                      <v-chip small :class="`${enrolment.status}`">{{
+                        enrolment.status
+                      }}</v-chip>
+                    </v-card-title>
 
-  <v-layout row wrap >
-  <v-flex sm6 lg6  v-for="enrolment in enrolments" :key="enrolment._id">
-  
-  <v-card 
-      elevation="2"
-      outlined  
-      class=" ma-5"
-      max-width="500"
-    
-  >
-     <router-link style="text-decoration: none; color: inherit;"
-     :to="{name: 'enrolments_show', params: {id: enrolment.id}}">
-    <v-card-title  class ="d-flex  justify-space-between">
-      {{enrolment.course.title}}
-      <v-chip small :class="`${enrolment.status}`">{{enrolment.status}}</v-chip>
-    </v-card-title>
-
-     <div class ="d-flex  justify-start">
-            <v-card-text  >
-            <div class="textStyle">Enrolment ID:</div>
-            {{enrolment.id}}
-            </v-card-text>
-            <v-card-text  >
-            <div class="textStyle">Course ID:</div>
-            {{enrolment.course_id}}
-            </v-card-text>
-             <v-card-text >
-            <div class="textStyle">Lecturer ID:</div>
-             {{enrolment.lecturer_id}}
-            </v-card-text>
-     </div>
-      </router-link>
-
- 
-  </v-card>
-
-  </v-flex>
-</v-layout>
-
-
-      </v-list-item-content>
-    </v-list-item>
-
- 
-  </v-card>
-
-  </v-container>
-</div>
+                    <div class="d-flex justify-start">
+                      <v-card-text>
+                        <div class="textStyle">Enrolment ID:</div>
+                        {{ enrolment.id }}
+                      </v-card-text>
+                      <v-card-text>
+                        <div class="textStyle">Course ID:</div>
+                        {{ enrolment.course_id }}
+                      </v-card-text>
+                      <v-card-text>
+                        <div class="textStyle">Lecturer ID:</div>
+                        {{ enrolment.lecturer_id }}
+                      </v-card-text>
+                    </div>
+                  </router-link>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </v-container>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import DeletePopUp from "@/components/DeletePopUp"
+import axios from "axios";
+import DeletePopUp from "@/components/DeletePopUp";
 
 export default {
   name: "LecturersShow",
   components: {
-    DeletePopUp
+    DeletePopUp,
   },
-   data() {
-       return {
-            lecturer: {},
-            enrolments: [],
-            type: "lecturer",
-            errors: "",
-            button: {
-              id: this.$route.params.id,
-              class: "button pa-5",
-              message: "Are you sure you want to delete this lecturer?",
-            }
-        
-       }
-   },
-   mounted() {
-       this.getLecturer()
-       this.getEnrolments()
- 
-   },
-   methods: {
-       getLecturer() {
-         let token = localStorage.getItem('token')
-           axios
-           .get(`https://college-api-mo.herokuapp.com/api/lecturers/${this.$route.params.id}`,
-           {
-               headers: {"Authorization" : `Bearer ${token}`}
-           })
-           .then(response => {
-               console.log(response)
-               this.lecturer = response.data.data
-             
-           })
-           .catch(error => {
-             console.log(error)
-            //  localStorage.removeItem('token')
-            //  this.$emit('invalid-token')
-             })
-       },
-       getEnrolments() {
-         let token = localStorage.getItem('token')
-           axios
-           .get(`https://college-api-mo.herokuapp.com/api/lecturers/${this.$route.params.id}`,
-           {
-               headers: {"Authorization" : `Bearer ${token}`}
-           })
-           .then(response => {
-               console.log(response)
-               this.enrolments = response.data.data.enrolments
-                 if (this.enrolments.length > 0) {
-                 this.button.message = "Deleting this lecturer will delete all enrolments related to the lecturer, are you sure you want to delete it?"
-               }
-           })
-           .catch(error => {
-             console.log(error)
-  
-             })
-       },
-   },
-
-  };
+  data() {
+    return {
+      lecturer: {},
+      enrolments: [],
+      type: "lecturer",
+      errors: "",
+      button: {
+        id: this.$route.params.id,
+        class: "button pa-5",
+        message: "Are you sure you want to delete this lecturer?",
+      },
+    };
+  },
+  mounted() {
+    this.getLecturer();
+    this.getEnrolments();
+  },
+  methods: {
+    getLecturer() {
+      let token = localStorage.getItem("token");
+      axios
+        .get(
+          `https://college-api-mo.herokuapp.com/api/lecturers/${this.$route.params.id}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.lecturer = response.data.data;
+        })
+        .catch((error) => {
+          console.log(error);
+          //  localStorage.removeItem('token')
+          //  this.$emit('invalid-token')
+        });
+    },
+    getEnrolments() {
+      let token = localStorage.getItem("token");
+      axios
+        .get(
+          `https://college-api-mo.herokuapp.com/api/lecturers/${this.$route.params.id}`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          this.enrolments = response.data.data.enrolments;
+          if (this.enrolments.length > 0) {
+            this.button.message =
+              "Deleting this lecturer will delete all enrolments related to the lecturer, are you sure you want to delete it?";
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
 </script>
 <style scoped>
-.button{
-   margin-left: 20px;
-   margin-bottom: 10px;
+.button {
+  margin-left: 20px;
+  margin-bottom: 10px;
 }
 
-.buttonEdit{
-   background: #f9aa33 !important;
+.buttonEdit {
+  background: #f9aa33 !important;
 }
 
-.buttonDelete{
-   background: red !important;
+.buttonDelete {
+  background: red !important;
 }
 
-.subheading{
+.subheading {
   font-weight: bold;
-  margin-top:10px;
-  margin-bottom:5px;
+  margin-top: 10px;
+  margin-bottom: 5px;
   color: black;
 }
-.fontSize{
+.fontSize {
   font-size: 20px;
   margin: 10px;
 }
-.textStyle{
+.textStyle {
   font-weight: bold;
   text-decoration: none;
 }
 
-.v-chip.interested{
+.v-chip.interested {
   background: #f9aa33 !important;
 }
 
-.v-chip.assigned{
-  background: #82b1ff!important;
+.v-chip.assigned {
+  background: #82b1ff !important;
 }
 
-.v-chip.career_break{
+.v-chip.career_break {
   background: red !important;
 }
 
-.v-chip.associate{
+.v-chip.associate {
   background: #4caf50 !important;
 }
 </style>
